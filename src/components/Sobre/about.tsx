7640704e4b056d35a5b7a74551743adc91054b33
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2022-12-14 22:29:46 -0300

    ajustando a section sobre

--- src/components/Sobre/about.tsx
+++ src/components/Sobre/about.tsx
@@ -1,6 +1,6 @@
-import { Sobre, SText, SImg } from '../../style/styleAbout';
+import { Sobre, SText, SImg, SSection } from '../../style/styleAbout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faDog, faEarth } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faDog, faEarth, faLeaf, faShield, faMicrochip } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
     return (  
@@ -12,9 +12,39 @@ const About = () => {
             </SImg>
             <SText>
                 <h2>Olá, tudo bem?</h2>
-                <p>Meu nome é Louise, sou farmacêutica de formação e especializada em cosmética natural. Comecei amanipular meus próprios cosméticos devido ao alto índice de matérias primas prejudiciais ao nossocorpo e ao meio ambiente. Substâncias altamente alergênicas e tóxicas que podem causarproblemas na pele, distúrbios endócrinos e destruição do meio ambiente. A manipulação decosméticos sempre foi minha paixão a qual estendo a todos que possuem necessidades especiais ouque apenas buscam um produto natural, seguro, eficaz e com alta tecnologia. Meus cosméticos sãoveganos livres de Parabenos, Ftalatos, Corantes e Essências sintéticas, Fenoxietanol, Silicones, Liberadores de formol, BHT, BHA, PEG’s, EDTA, derivados de animais e substâncias nocivas à pele eao meio ambiente.</p>
+                <p>Meu nome é Louise, sou farmacêutica de formação e especializada em cosmética natural. Comecei a manipular meus próprios cosméticos devido ao alto índice de matérias primas prejudiciais ao nosso corpo e ao meio ambiente. Substâncias altamente alergênicas e tóxicas que podem causar problemas na pele, distúrbios endócrinos e destruição do meio ambiente. A manipulação de cosméticos sempre foi minha paixão a qual estendo a todos que possuem necessidades especiais ou que apenas buscam um produto natural, seguro, eficaz e com alta tecnologia. Meus cosméticos são veganos livres de Parabenos, Ftalatos, Corantes e Essências sintéticas, Fenoxietanol, Silicones, Liberadores de formol, BHT, BHA, PEG’s, EDTA, derivados de animais e substâncias nocivas à pele e ao meio ambiente.</p>
                 <span>By Lou, pensando em você, nos animais e no planeta! <FontAwesomeIcon className='icon' icon={faHeart} /><FontAwesomeIcon className='icon' icon={faDog} /><FontAwesomeIcon className='icon' icon={faEarth} /></span>
             </SText>
+            <SSection>
+                <div className="item">
+                    <span><FontAwesomeIcon className='icon' icon={faLeaf} /></span>
+                    <div className="text">
+                        <p>Produto</p>
+                        <h3>Natural</h3>
+                    </div>
+                </div>
+                <div className="item">
+                    <span><FontAwesomeIcon className='icon' icon={faShield} /></span>
+                    <div className="text">
+                        <p>Sendo 100%</p>
+                        <h3>Seguro</h3>
+                    </div>
+                </div>
+                <div className="item">
+                    <span></span>
+                    <div className="text">
+                        <p>E 100%</p>
+                        <h3>Eficaz</h3>
+                    </div>
+                </div>
+                <div className="item">
+                    <span><FontAwesomeIcon className='icon' icon={faMicrochip} /></span>
+                    <div className="text">
+                        <p>Usando uma</p>
+                        <h3>Alta tecnologia</h3>
+                    </div>
+                </div>
+            </SSection>
         </Sobre>
     );
 }
--- src/style/styleAbout.tsx
+++ src/style/styleAbout.tsx
@@ -3,6 +3,45 @@ import styled from "styled-components";
 export const Sobre = styled.section`
     display: flex;
     align-items: center;
+    flex-wrap: wrap;
+`;
+
+export const SSection = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin: 40px 0px;
+
+    .item {
+        width: 240px;
+        height: 80px;
+        background-color: var(--grey);
+        border-radius: 15px;
+        padding: 10px;
+        margin: 0px 30px;
+        display: flex;  
+        align-items: center;
+        justify-content: center;
+
+        h3 {
+            color: var(--green2);
+        }
+
+        .icon {
+            font-size: 52px;
+            color: var(--green2);
+            margin: 0px 10px;
+        }
+
+        .text {
+            display: flex;
+            flex-wrap: wrap;
+
+            p {
+                width: 100%;
+            }
+        }
+    }
 `;
 
 export const SText = styled.div`
@@ -26,6 +65,7 @@ export const SText = styled.div`
 
 export const SImg = styled.div`
     width: 50%;
+    flex: 1;
 
     .bloob {
         width: 550px;  
